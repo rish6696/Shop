c84fcbbd07ffd3e6b7f71a6a7db6cee799f24d99
Rishabh sharma <rish6696.rs@gmail.com>
2019-03-21 21:04:35 +0530

    all three working

--- routes/Products.js
+++ routes/Products.js
@@ -1,9 +1,28 @@
 const express=require('express');
 const route=express.Router();
+const{
+    Products
+}=require('../db');
 
 route.get('/',async(req,res)=>{
 
 
 })
+
+
+route.post('/', async (req, res) => {
+    console.log(Products);
+
+    const newProd = await Products.create({
+      name: req.body.name,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      vendorId: req.body.vendorId,
+      
+    })
+  
+    res.status(201).send(newProd)
+  
+  })
 
 module.exports=route;
--- routes/Users.js
+++ routes/Users.js
@@ -1,9 +1,36 @@
 const express=require('express');
 const route=express.Router();
 
+const{
+    Users
+}=require('../db')
+
 route.get('/',async(req,res)=>{
 
+ const users= await Users.findAll(); 
+ res.send(users);
 
+})
+
+route.post('/',async (req,res)=>{
+    try{
+
+    const user=await Users.create({
+        name:req.body.name,
+        city:req.body.city
+    })
+    res.status(200).send(user)
+   }
+   catch(err)
+   {
+       res.status(400).send(
+           {
+               message:err.message
+
+
+           }
+       )
+   }
 })
 
 module.exports=route;
